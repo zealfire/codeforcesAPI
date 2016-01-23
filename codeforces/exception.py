dd6ef99d3bf785ab60bb784c1f4e5b9bc98cef88
zealfire <iamalok185@gmail.com>
2016-01-24 02:05:38 +0530

    Some more tests added and exception class made.

--- codeforces/exception.py
+++ codeforces/exception.py
@@ -5,3 +5,8 @@ class exception:
 		''''''
 
 	def unwanted(self, exception):
+		exception = json.loads(exception)
+		if exception["status"] != "OK":
+			return exception["comment"]
+		else:
+			return "API working fine."
--- test/test.py
+++ test/test.py
@@ -3,10 +3,21 @@ import sys
 sys.path.insert(0,'../')
 
 from codeforces.codeforces import codeforces
+from codeforces.exception import exception
 
 cd = codeforces()
+ex = exception()
 print cd.hacks(374).text
 
 print cd.ratingChanges(374).text
 
 print cd.standings(contestId=374,from=1,count=5,showUnofficial='true').url
+
+checking working of exception API
+r= cd.standings(contestId=374, count=5, showUnofficial='true')
+
+if(r.status_code != 202):
+	print ex.unwanted(cd.standings(contestId=374, count=5, showUnofficial='true').text)
+else:
+	print cd.standings(contestId=374, count=5, showUnofficial='true').text
+
