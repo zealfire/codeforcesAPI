8867311d728c7c561285249091e9470842d24715
zealfire <iamalok185@gmail.com>
2016-01-24 02:41:55 +0530

    Install file modifies.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setuptools
+from setuptools import setup
 
 setup(
 	name='codeforcesAPI',
-	version='0.0.1'
+	version='0.0.1',
 	author="Alok Pandey",
 	author_email="iamalok185@gmail.com",
-	packages=['codeforcesAPI', 'test', ],
+	packages=['codeforces', 'test', ],
 	license='MIT',
 	description="Python wrapper for codeforces API.",
-	long_description=open('READ.md','r').read(),
+	long_description=open('README.md','r').read(),
 	install_requires=['requests', ],
 	)
